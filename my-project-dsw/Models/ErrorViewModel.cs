6604eb4e9fd79b21898cff4071dd58920d3a8973
Jean Huaringa <jeanhuaringach@gmail.com>
2025-01-16 10:49:52 -0500

    Initial commit for ASP.NET Core MVC Project

---
+++ my-project-dsw/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace my_project_dsw.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
